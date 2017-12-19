d6d473d3d17d5321b34bd6d1104ff2bea57d3988
Martin Sandelin <mortin_1993@hotmail.com>
2017-12-19 13:37:22 +0100

    Hej hej

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Terminal terminal = TerminalFacade.createTerminal(System.in, System.out, Charset.forName(“UTF8”));
+
+        
+    }
+}
