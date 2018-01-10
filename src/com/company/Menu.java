8f8229ffb29b909728bb22f3b2f41f8135ae9717
Marie-Charlotte Persson <mariecharlotte.persson@gmail.com>
2018-01-10 22:33:55 +0100

    Added a menu class

---
+++ src/com/company/Menu.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Menu {
+
+    public void Run() {
+
+        int playGame = 0;
+
+        do {
+            switch (playGame) {
+                case 1:
+                    // Draw game board and start game
+                    break;
+                case 2:
+                    // Look at high scores
+                    break;
+                case 3:
+                    // Get help on how to move
+                    break;
+                case 4:
+                    // Exit game using terminal.exit(0);
+                    break;
+                default:
+                    //Handle in case some one presses the wrong numbers?
+            }
+
+        } while (playGame != 3);
+
+    }
+
+}
